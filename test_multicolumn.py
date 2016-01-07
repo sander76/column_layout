91e7bd3e87106a24a4db722de151d7589047188c
sander <s.teunissen@gmail.com>
2016-01-07 12:54:08 +0100

    extra readme

--- test_multicolumn.py
+++ test_multicolumn.py
@@ -4,4 +4,14 @@ from three_columns import ThreeColumnExtension
 def test_output1():
     input = "%1 test1 %4 test 4 "
     txt = markdown(input,extensions=[ThreeColumnExtension()])
-    assert txt == '<p>\n<div class="instruction bg-info row">\n<div class="col-md-1 col0">test1</div>\n<div class="col-md-4 col1">test 4</div>\n</div>\n</p>'
+    assert txt == '<p>\n<div class="instruction bg-info row">\n<div class="col-md-1 col0">test1</div>\n<div class="col-md-4 col1">test 4 </div>\n</div>\n</p>'
+
+def test_output2():
+    input = "%1 test1 %4 value = 10%"
+    txt = markdown(input,extensions=[ThreeColumnExtension()])
+    assert txt == '<p>\n<div class="instruction bg-info row">\n<div class="col-md-1 col0">test1</div>\n<div class="col-md-4 col1">value = 10%</div>\n</div>\n</p>'
+
+def test_output3():
+    input = "%1 test1 %4 test 4 \n%1 test1 %4 test 4 "
+    txt = markdown(input,extensions=[ThreeColumnExtension()])
+    assert txt == '<p>\n<div class="instruction bg-info row">\n<div class="col-md-1 col0">test1</div>\n<div class="col-md-4 col1">test 4 </div>\n</div>\n</p>'
--- three_columns.py
+++ three_columns.py
@@ -1,50 +1,51 @@
 from markdown.extensions import Extension
 from markdown.inlinepatterns import Pattern
 from markdown.util import etree
-
+import re
 
 def makeExtension(*args, **kwargs):
     return ThreeColumnExtension(*args, **kwargs)
 
 
-THREECOL = r'(\%\d\d{0,1}\s[^\%]+)(\%\d\d{0,1}\s[^\%]+|.*)(\%\d\d{0,1}\s[^\%]+|.*)(\%\d\d{0,1}\s[^\%]+|.*)(\%\d\d{0,1}\s[^\%]+|.*)(\%\d\d{0,1}\s[^\%]+|.*)'
-
+#THREECOL = r'(\%\d\d{0,1}\s[^\%]+)(\%\d\d{0,1}\s[^\%]+|.*)(\%\d\d{0,1}\s[^\%]+|.*)(\%\d\d{0,1}\s[^\%]+|.*)(\%\d\d{0,1}\s[^\%]+|.*)(\%\d\d{0,1}\s[^\%]+|.*)'
+THREECOL = r'\%\d\d{0,1}\s.*?\n'
+ALLCOLS = re.compile(r'\%(\d{1,2})\s(.+?)(?=\s\%\d|\Z)')
+#ALLCOLS = re.compile(r"\%{1}(\d{1,2})")
 
 class ThreeColumnPattern(Pattern):
     def handleMatch(self, m):
-        #line = m.string
-        #self._set_columns(line)
+        line = m.string
+        self._set_columns(line)
 
         # row column
         el1 = etree.Element("div")
         el1.set('class', 'instruction bg-info row')
 
-        groupnr=1
-        match = self._get_group(m,groupnr)
-        while match:
-            print()
-            groupnr+=1
-            match = self._get_group(m,groupnr)
-        for idx,match in m.group:
         # create the columns.
-        #for idx, col in enumerate(self.cols):
-            width, content = self._parse_column(col)
+        for idx,column in enumerate(self.cols):
+            width,content=column
             _col = etree.SubElement(el1, "div")
             _col.set('class','col-md-{} col{}'.format(width,idx))
             _col.text = content
 
         return el1
 
-    def _get_group(self,m,groupnr):
-        match = m.group(groupnr)
-        return match
+    # def _get_group(self,m,groupnr):
+    #     try:
+    #         match = m.group(groupnr+2)  # first group match is group +2
+    #         return match
+    #     except IndexError:
+    #         message = "error parsing: {}".format(m.string)
+    #         raise IndexError(message)
 
     def _set_columns(self, line):
-        self.cols = line.split('%')[1:]
+        _cols = re.findall(ALLCOLS,line)
+        #_cols=re.split(ALLCOLS,line)
+        self.cols = _cols
 
     def _parse_column(self, column):
         width, content = column.split(maxsplit=1)
-        return width, content.strip()
+        return width[1:], content.strip()
 
         # def _get_col_width(self, width):
         #     _width = width[-1]
